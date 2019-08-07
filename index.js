d90ed114b41c15cc1131b77754244cf1110ee383
SaadRajpoot <53665598+SaadRajpoot@users.noreply.github.com>
2019-08-07 18:39:29 +0500

    !st upload.

---
+++ index.js
@@ -0,0 +1,48 @@
+var NumOfDrums = document.querySelectorAll(".drum").length;
+
+
+
+for (var i = 0; i < NumOfDrums; i++) {
+  document.querySelectorAll("button")[i].addEventListener("click", function() {
+    var exp = this.innerHTML;
+    handleEvent(exp);
+  });
+
+}
+  document.addEventListener("keypress", function(event) {
+    handleEvent(event.key);
+  });
+
+  function handleEvent(exp) {
+    var audio;
+
+    switch (exp) {
+      case "w":
+        audio = new Audio("sounds/tom-1.mp3");
+        break;
+      case "a":
+        audio = new Audio("sounds/tom-2.mp3");
+        break;
+      case "s":
+        audio = new Audio("sounds/tom-3.mp3");
+        break;
+      case "d":
+        audio = new Audio("sounds/tom-4.mp3");
+        break;
+      case "j":
+        audio = new Audio("sounds/snare.mp3");
+        break;
+      case "k":
+        audio = new Audio("sounds/crash.mp3");
+        break;
+      case "l":
+        audio = new Audio("sounds/kick-bass.mp3");
+        break;
+      default:
+        console.log("Wrong button pressed.");
+        break;
+
+    }
+
+    audio.play();
+  }
