f67951bced6ba9416801a40b7eeeed4632e0d8aa
SaadRajpoot <53665598+SaadRajpoot@users.noreply.github.com>
2019-08-07 19:03:32 +0500

    Animation added.

--- index.js
+++ index.js
@@ -6,43 +6,54 @@ for (var i = 0; i < NumOfDrums; i++) {
   document.querySelectorAll("button")[i].addEventListener("click", function() {
     var exp = this.innerHTML;
     handleEvent(exp);
+    buttonAnimation(exp);
   });
 
 }
-  document.addEventListener("keypress", function(event) {
-    handleEvent(event.key);
-  });
+document.addEventListener("keypress", function(event) {
+  handleEvent(event.key);
+  buttonAnimation(event.key);
+});
 
-  function handleEvent(exp) {
-    var audio;
+function handleEvent(exp) {
+  var audio;
 
-    switch (exp) {
-      case "w":
-        audio = new Audio("sounds/tom-1.mp3");
-        break;
-      case "a":
-        audio = new Audio("sounds/tom-2.mp3");
-        break;
-      case "s":
-        audio = new Audio("sounds/tom-3.mp3");
-        break;
-      case "d":
-        audio = new Audio("sounds/tom-4.mp3");
-        break;
-      case "j":
-        audio = new Audio("sounds/snare.mp3");
-        break;
-      case "k":
-        audio = new Audio("sounds/crash.mp3");
-        break;
-      case "l":
-        audio = new Audio("sounds/kick-bass.mp3");
-        break;
-      default:
-        console.log("Wrong button pressed.");
-        break;
+  switch (exp) {
+    case "w":
+      audio = new Audio("sounds/tom-1.mp3");
+      break;
+    case "a":
+      audio = new Audio("sounds/tom-2.mp3");
+      break;
+    case "s":
+      audio = new Audio("sounds/tom-3.mp3");
+      break;
+    case "d":
+      audio = new Audio("sounds/tom-4.mp3");
+      break;
+    case "j":
+      audio = new Audio("sounds/snare.mp3");
+      break;
+    case "k":
+      audio = new Audio("sounds/crash.mp3");
+      break;
+    case "l":
+      audio = new Audio("sounds/kick-bass.mp3");
+      break;
+    default:
+      console.log("Wrong button pressed.");
+      break;
 
-    }
-
-    audio.play();
   }
+
+  audio.play();
+}
+
+function buttonAnimation(key) {
+  var button= document.querySelector("." + key);
+button.classList.add("pressed");
+  setTimeout(function() {
+    button.classList.remove("pressed");
+  },100);
+
+}
